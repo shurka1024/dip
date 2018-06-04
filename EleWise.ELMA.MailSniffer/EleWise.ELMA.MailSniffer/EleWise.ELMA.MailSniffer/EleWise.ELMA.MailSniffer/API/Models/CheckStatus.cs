f2d592a5f7ba15e4d7edcf2ffeec190d97162be1
a.maksimova <a.maksimova@elewise.com>
2018-06-04 13:34:21 +0400

    Добавлена обработка файлов, чтение содержимого Добавить ссылку на "API\Models\CheckStatus.cs"

---
+++ EleWise.ELMA.MailSniffer/EleWise.ELMA.MailSniffer/EleWise.ELMA.MailSniffer/EleWise.ELMA.MailSniffer/API/Models/CheckStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EleWise.ELMA.MailSniffer.API.Models
+{
+    /// <summary>
+    /// Статус результат проверки
+    /// </summary>
+    public enum CheckStatus
+    {
+        Default = 0,
+        Ok = 1,
+        Warning = 2,
+        Stop = 3
+    }
+}
